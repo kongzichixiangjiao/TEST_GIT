7ddbe1c17af114697e2b5dad1cb804e580036b4e
kongzichixiangjiao <58895347@qq.com>
2016-01-16 16:35:29 +0800

    Initial Commit

---
+++ TEST_GIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST_GIT
+//
+//  Created by houjianan on 16/1/16.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TEST_GIT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TEST_GIT
+//
+//  Created by houjianan on 16/1/16.
+//  Copyright © 2016年 houjianan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
